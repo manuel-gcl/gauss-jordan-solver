8ede836cc2043d4862351bcd931361866325ea8d
manuel-gcl <manuelgonzalez2849@gmail.com>
2024-07-24 19:32:57 -0300

    Update code style

--- tests/test_matrix_solution.py
+++ tests/test_matrix_solution.py
@@ -21,16 +21,16 @@ class TestSolverResults:
             matrix = np.random.randint(100, size=(row, col))
             self.matrix_set.append(np.ndarray.tolist(matrix))
             amount -= 1
-    
+
     def get_coefficient_and_dependent_matrix(self, matrix):
         coefficient_matrix = []
         dependent_matrix = []
-        for row in matrix: 
-            coefficient_matrix.append(row[:-1]) 
+        for row in matrix:
+            coefficient_matrix.append(row[:-1])
             dependent_matrix.append(row[-1])
-        
+
         return coefficient_matrix, dependent_matrix
-    
+
     def get_res_with_gauss_jordan(self, matrix):
         solver = GaussJordanSolver(matrix)
         solver.solve_matrix()
@@ -38,38 +38,37 @@ class TestSolverResults:
         dep_matrix = [float(num) for num in dep_matrix]
 
         return dep_matrix
-    
+
     def get_res_with_numpy(self, coef_mat, dep_mat):
         inv_coef_mat = np.linalg.inv(coef_mat)
         res = np.matmul(inv_coef_mat, dep_mat)
         res = np.ndarray.tolist(res)
-            
+
         return res
-    
-    def compare_results(self, matrixA, matrixB):
+
+    def compare_results(self, matrix_a, matrix_b):
         res = True
-        matrixA.sort()
-        matrixB.sort()
-        
-        for index, num in enumerate(matrixA):
-            if round(num, 5) != round(matrixB[index], 5):
+        matrix_a.sort()
+        matrix_b.sort()
+
+        for index, num in enumerate(matrix_a):
+            if round(num, 5) != round(matrix_b[index], 5):
                 res = False
                 break
 
         return res
-    
+
 if __name__ == "__main__":
-    d = TestSolverResults()
-    d._generate_matrix_d(100, 5, 4)
-    
-    for m in d.matrix_set:
-        coef_m, dep_m = d.get_coefficient_and_dependent_matrix(m)
-        np_res = d.get_res_with_numpy(np.array(coef_m), np.array(dep_m))
+    tester = TestSolverResults()
+    tester._generate_matrix_d(100, 5, 4)
 
-        gaussJordan_res = d.get_res_with_gauss_jordan(m)
+    for m in tester.matrix_set:
+        coef_m, dep_m = tester.get_coefficient_and_dependent_matrix(m)
+        np_res = tester.get_res_with_numpy(np.array(coef_m), np.array(dep_m))
 
-        res = d.compare_results(np_res, gaussJordan_res)
-        if not res:
+        gaussJordan_res = tester.get_res_with_gauss_jordan(m)
+
+        if not tester.compare_results(np_res, gaussJordan_res):
             print("Square Matrix has different results\n")
             print(f"GJ P Method: {gaussJordan_res}\n")
             print(f"Numpy Method {np_res}\n")
